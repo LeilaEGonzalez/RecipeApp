b2dfa9610616ff5549b9928f4276d5fdb9f9038e
Leila Elizabeth Gonzalez <layla.elizabeth95@gmail.com>
2022-04-18 17:55:47 -0300

    finished slider and aesthetics

--- js/index.js
+++ js/index.js
@@ -1,19 +1,30 @@
 const addButton = document.querySelector("#add-btn");
-const sliderBurger = document.querySelector("#slider_burger");
-const detailedRecipe = document.querySelector("#detailed-recipe");
+const sidebar = document.querySelector("#sidebar");
+const navToggle = document.querySelector("#toggle-btn");
 const modalForm = document.querySelector("#modal-form");
 const titleForm = document.querySelector("#title-form");
 const sourceForm = document.querySelector("#source-form");
 const pictureForm = document.querySelector("#picture-form");
 const ingredientsForm = document.querySelector("#ingredients-form");
 
-const show = () => {
-  document.getElementById("sidebar").classList.toggle("active");
-};
+navToggle.addEventListener('click', () => {
+  const visibility = sidebar.getAttribute('data-visible')
+  if (visibility === "false") {
+    sidebar.setAttribute('data-visible', true);
+    navToggle.setAttribute('aria-expanded', true);
+    navToggle.style.background = "url(/assets/img/exit.svg)"
+    navToggle.style.backgroundRepeat = "no-repeat";
+  } else if (visibility === "true") {
+    sidebar.setAttribute('data-visible', false);
+    navToggle.setAttribute('aria-expanded', false);
+    navToggle.style.background = "url(/assets/img/burger.svg)"
+  }
+})
 
 addButton.addEventListener("click", (event) => {
   event.preventDefault();
   recipeForm();
+
 });
 
 const recipeForm = () => {
@@ -22,45 +33,45 @@ const recipeForm = () => {
 
 const form = `    <div id="overlay"></div>
 <div class="modal-container" id="form-container">
-            <div class="modal-header">
+    <div class="modal-header">
         <div class="modal-title">New Recipe</div>
         <button class="close-button">&times;</button>
     </div>
     <div class="modal-body">
-        <form class="form" id="form">
+        <form class="form" id="modal-form">
             <div class="form-box">
-            <label>Title</label>
-            <input type="text" placeholder="Write Here..." id="title-form">
-            <i class="fa-solid fa-circle-exclamation"></i>
-            <i class="fa-solid fa-circle-check"></i>
-            <small>Error</small>
-        </div>
+                <label>Title</label>
+                <input type="text" placeholder="Write Here..." id="title-form">
+                <i class="fa-solid fa-circle-exclamation"></i>
+                <i class="fa-solid fa-circle-check"></i>
+                <small>Error</small>
+            </div>
 
-        <div class="form-box">
-            <label>Source</label>
-            <input type="text" placeholder="Write Here..." id="source-form">
-            <i class="fa-solid fa-circle-exclamation"></i>
-            <i class="fa-solid fa-circle-check"></i>
-            <small>Error</small>
-        </div>
+            <div class="form-box">
+                <label>Source</label>
+                <input type="text" placeholder="Write Here..." id="source-form">
+                <i class="fa-solid fa-circle-exclamation"></i>
+                <i class="fa-solid fa-circle-check"></i>
+                <small>Error</small>
+            </div>
 
-        <div class="form-box">
-            <label>Picture</label>
-            <input type="text" placeholder="" id="picture-form">
-            <i class="fa-solid fa-file-circle-plus"></i>
-            <i class="fa-solid fa-circle-check"></i>
-            <small>Error</small>
-        </div>
-
-        <div class="form-box">
-            <label>Ingredients</label>
-            <input class="ingredients-input" type="text" placeholder="Write Here..." id="ingredients-form">
-            <i class="fa-solid fa-circle-exclamation"></i>
-            <i class="fa-solid fa-circle-check"></i>
-            <small>Error</small>
-        </div>
-
-        <button class="form-btn">Submit</button>
+            
+            <div class="form-box">
+                <label>Ingredients</label>
+                <input type="text" placeholder="Write Here..." id="ingredients-form">
+                <i class="fa-solid fa-circle-exclamation"></i>
+                <i class="fa-solid fa-circle-check"></i>
+                <small>Error</small>
+            </div>
+            
+            <div class="form-box">
+                <label for="picture-recipe">Picture</label>
+                <input type="file" id="recipe-image" name="file" accept="image/png, image/jpeg">
+                <i class="fa-solid fa-file-circle-plus"></i>
+                <i class="fa-solid fa-circle-check"></i>
+                <small>Error</small>
+            </div>
+            <button class="form-btn">Submit</button>
 
         </form>
     </div>
@@ -70,6 +81,7 @@ modalForm.addEventListener("submit", (event) => {
   event.preventDefault();
   checkInputs();
 });
+
 
 const checkInputs = () => {
   validate(titleForm);
@@ -101,3 +113,6 @@ const setSuccessFor = (input) => {
   const formBox = input.parentElement;
   formBox.className = "form-box success";
 };
+
+const imageRecipe = document.querySelector('#recipe-image')
+imageRecipe.addEventListener('change', updateImageDisplay);
